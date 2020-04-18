4915229e61d6240403f34a016bbcc15443f84e43
prince3103 <prince.sethi@trantorinc.com>
2020-04-18 23:32:41 +0530

    updated init.py file for database

--- puppycompanyblog/__init__.py
+++ puppycompanyblog/__init__.py
@@ -22,7 +22,7 @@ app.config['SECRET_KEY'] = 'mysecret'
 ###############################
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///'+ os.path.join(basedir, 'data.sqlite'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
